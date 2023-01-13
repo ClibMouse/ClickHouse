d7c04c2aa00471ff47264cccd53e7f32da8123f4
Larry Luo <larry.luo@ca.ibm.com>
2023-01-12 23:35:20 -0500

    match master

--- tests/ci/fast_test_check.py
+++ tests/ci/fast_test_check.py
@@ -5,16 +5,13 @@ import subprocess
 import os
 import csv
 import sys
+import atexit
+from pathlib import Path
+from typing import List, Tuple
 
 from github import Github
 
-from env_helper import CACHES_PATH, TEMP_PATH, GITHUB_SERVER_URL, GITHUB_REPOSITORY
-from pr_info import FORCE_TESTS_LABEL, PRInfo, SKIP_SIMPLE_CHECK_LABEL
-from s3_helper import S3Helper
-from get_robot_token import get_best_robot_token
-from upload_result_helper import upload_results
-from docker_pull_helper import get_image_with_version
-from commit_status_helper import post_commit_status, get_commit
+from ccache_utils import get_ccache_if_not_exists, upload_ccache
 from clickhouse_helper import (
     ClickHouseHelper,
     mark_flaky_tests,
@@ -35,7 +32,10 @@ from stopwatch import Stopwatch
 from tee_popen import TeePopen
 from upload_result_helper import upload_results
 
-NAME = "Fast test (actions)"
+NAME = "Fast test"
+
+# Will help to avoid errors like _csv.Error: field larger than field limit (131072)
+csv.field_size_limit(sys.maxsize)
 
 
 def get_fasttest_cmd(
@@ -100,7 +100,9 @@ def main():
 
     pr_info = PRInfo()
 
-    gh = Github(get_best_robot_token())
+    gh = Github(get_best_robot_token(), per_page=100)
+
+    atexit.register(update_mergeable_check, gh, pr_info, NAME)
 
     rerun_helper = RerunHelper(gh, pr_info, NAME)
     if rerun_helper.is_already_finished_by_status():
@@ -227,18 +229,6 @@ def main():
         if FORCE_TESTS_LABEL in pr_info.labels and state != "error":
             print(f"'{FORCE_TESTS_LABEL}' enabled, will report success")
         else:
-            if SKIP_SIMPLE_CHECK_LABEL not in pr_info.labels:
-                url = (
-                    f"{GITHUB_SERVER_URL}/{GITHUB_REPOSITORY}/"
-                    "blob/master/.github/PULL_REQUEST_TEMPLATE.md?plain=1"
-                )
-                commit = get_commit(gh, pr_info.sha)
-                commit.create_status(
-                    context="Simple Check",
-                    description=f"{NAME} failed",
-                    state="failed",
-                    target_url=url,
-                )
             sys.exit(1)
 
 
