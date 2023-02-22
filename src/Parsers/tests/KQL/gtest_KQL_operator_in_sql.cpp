985ea6b6321425ecf3bc5e066bca558b19e5d3f6
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2023-02-22 08:17:25 -0800

    Update unit tests

--- src/Parsers/tests/KQL/gtest_KQL_operator_in_sql.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_operator_in_sql.cpp
@@ -33,11 +33,11 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery_operator_in_sql, ParserKQLTest,
         },
         {
             "select * from kql(Customers | where FirstName !has 'Peter');",
-            "SELECT *\nFROM\n(\n    SELECT *\n    FROM Customers\n    WHERE NOT ifNull(hasTokenCaseInsensitiveOrNull(FirstName, 'Peter'), FirstName = 'Peter')\n)"
+            "SELECT *\nFROM\n(\n    SELECT *\n    FROM Customers\n    WHERE NOT ifNull(hasTokenCaseInsensitiveOrNull(FirstName, 'Peter'), hasTokenCaseInsensitive(FirstName, 'Peter') AND (positionCaseInsensitive(FirstName, 'Peter') > 0))\n)"
         },
         {
             "select * from kql(Customers | where FirstName !has_cs 'peter');",
-            "SELECT *\nFROM\n(\n    SELECT *\n    FROM Customers\n    WHERE NOT ifNull(hasTokenOrNull(FirstName, 'peter'), FirstName = 'peter')\n)"
+            "SELECT *\nFROM\n(\n    SELECT *\n    FROM Customers\n    WHERE NOT ifNull(hasTokenOrNull(FirstName, 'peter'), hasToken(FirstName, 'peter') AND (position(FirstName, 'peter') > 0))\n)"
         },
         {
             "select * from kql($$Customers | where FirstName !hasprefix 'Peter'$$);",
