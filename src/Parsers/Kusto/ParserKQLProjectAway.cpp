0c7a9457f9a151da8e0c08bda795ad3b04a0f9cb
Yong Wang <yong.wang@ibm.com>
2023-02-23 21:13:40 -0800

    Kusto-phase3 : Add test for projectaway

--- src/Parsers/Kusto/ParserKQLProjectAway.cpp
+++ src/Parsers/Kusto/ParserKQLProjectAway.cpp
@@ -1,8 +1,9 @@
-#include <Parsers/ExpressionListParsers.h>
-#include <Parsers/ParserSelectQuery.h>
-#include <Parsers/Kusto/ParserKQLQuery.h>
-#include <Parsers/Kusto/ParserKQLProjectAway.h>
 #include <format>
+#include <Parsers/ExpressionListParsers.h>
+#include <Parsers/Kusto/ParserKQLProjectAway.h>
+#include <Parsers/Kusto/ParserKQLQuery.h>
+#include <Parsers/ParserSelectQuery.h>
+#include <Parsers/ParserTablesInSelectQuery.h>
 #include "Utilities.h"
 namespace DB
 {
@@ -10,16 +11,24 @@ namespace ErrorCodes
 {
     extern const int SYNTAX_ERROR;
 }
-bool ParserKQLProjectAway::parseImpl(Pos & pos, ASTPtr & /*node*/, Expected &/* expected*/)
+bool ParserKQLProjectAway::parseImpl(Pos & pos, ASTPtr & node, Expected & /*expected*/)
 {
     size_t bracket_count = 0;
     auto begin_pos = pos;
-    String project_away_query;
-    String wildcard_column_str;
     String regular_column_str;
     std::vector<String> wildcard_columns;
     std::vector<String> regular_columns;
     ASTPtr sub_query_node;
+
+    auto append_columns = [&regular_columns, &wildcard_columns](Pos & begin, Pos & end)
+    {
+        const auto column = String(begin->begin, end->end);
+        const auto regex_column = WildcardToRegex(column);
+        if (regex_column == column)
+            regular_columns.push_back(column);
+        else
+            wildcard_columns.push_back("'" + regex_column + "'");
+    };
 
     while (!pos->isEnd() && pos->type != TokenType::PipeMark && pos->type != TokenType::Semicolon)
     {
@@ -33,25 +42,24 @@ bool ParserKQLProjectAway::parseImpl(Pos & pos, ASTPtr & /*node*/, Expected &/* 
         {
             auto end_pos = pos;
             --end_pos;
-            const auto column = String(begin_pos->begin, end_pos->end);
-            const auto regex_column = WildcardToRegex(column);
-            if (regex_column == column)
-                regular_columns.push_back(column);
-            else
-                wildcard_columns.push_back("'" + regex_column + "'");
+            append_columns(begin_pos, end_pos);
             begin_pos = pos;
             ++begin_pos;
         }
         ++pos;
     }
+
+    --pos;
+    append_columns(begin_pos, pos);
+
     if (wildcard_columns.empty() && regular_columns.empty())
         throw Exception("Syntax error: Missing projected away expressions", ErrorCodes::SYNTAX_ERROR);
 
     if (!regular_columns.empty())
     {
         if (regular_columns.size() == 1)
-            regular_column_str =  regular_columns[0];
-        else 
+            regular_column_str = regular_columns[0];
+        else
         {
             regular_column_str = "(" + regular_columns[0];
             for (size_t i = 1; i < regular_columns.size(); ++i)
@@ -62,20 +70,30 @@ bool ParserKQLProjectAway::parseImpl(Pos & pos, ASTPtr & /*node*/, Expected &/* 
         }
     }
 
-    String first_away = std::format("SELECT * EXCEPT {} from dummy", regular_columns.empty() ? wildcard_columns[0] : regular_column_str);
-
-    size_t wildcard_columns_index =  regular_columns.empty() ? 1 : 0;
+    size_t wildcard_columns_index = regular_columns.empty() ? 1 : 0;
 
     for (size_t i = wildcard_columns_index; i < wildcard_columns.size(); ++i)
     {
-        project_away_query = std::format("SELECT * EXCEPT {} FROM ({})", wildcard_columns[i], first_away);
-        first_away = project_away_query;
+        String project_away_query = std::format("(SELECT * EXCEPT {} FROM dummy_input)", wildcard_columns[i]);
+        if (!parseSQLQueryByString(std::make_unique<ParserTablesInSelectQuery>(), project_away_query, sub_query_node, pos.max_depth))
+            return false;
+        if (!setSubQuerySource(sub_query_node, node, true, i != wildcard_columns_index))
+            return false;
+        node = std::move(sub_query_node);
     }
 
-    if (!parseSQLQueryByString(std::make_unique<ParserSelectQuery>(), project_away_query, sub_query_node, pos.max_depth))
+    String last_away = std::format("SELECT * EXCEPT {} from dummy", regular_columns.empty() ? wildcard_columns[0] : regular_column_str);
+
+    if (!parseSQLQueryByString(std::make_unique<ParserSelectQuery>(), last_away, sub_query_node, pos.max_depth))
         return false;
-  //  sub_query_node->as<ASTSelectQuery>()->setExpression(ASTSelectQuery::Expression::TABLES, std::move(node));
-   // node = std::move(sub_query_node);
+    if (wildcard_columns_index < wildcard_columns.size())
+        sub_query_node->as<ASTSelectQuery>()->setExpression(ASTSelectQuery::Expression::TABLES, std::move(node));
+    else
+    {
+        if (!setSubQuerySource(sub_query_node, node, false, false))
+            return false;
+    }
+    node = std::move(sub_query_node);
 
     return true;
 }
