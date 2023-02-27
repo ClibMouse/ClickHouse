c45115e4e61ff9f49b0ef84de287b97b7db52991
Brian Mitchell <bemitch@us.ibm.com>
2023-02-27 14:44:50 -0800

    split executeImpl up to make it easier to understand

--- src/Functions/Kusto/kqlHasAnyIp.h
+++ src/Functions/Kusto/kqlHasAnyIp.h
@@ -18,50 +18,53 @@ namespace ErrorCodes
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
-enum ArgumentPolicy
+
+enum class ArgumentPolicy
 {
-    AP_Single,
-    AP_Variadic
+    Single,
+    Variadic
 };
 
-class KqlHasAnyIpArgument
+template <ArgumentPolicy ap>
+DataTypePtr getReturnType(const ColumnsWithTypeAndName & arguments, const std::string & name)
 {
-public:
-    static DataTypePtr ArgumentVerifier(const ColumnsWithTypeAndName & arguments, const std::string name, ArgumentPolicy ap)
+    const auto args_length = arguments.size();
+
+    if (args_length < 2)
     {
-        const auto args_length = arguments.size();
-
-        if (args_length < 2)
-        {
-            throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Number of arguments for function {} doesn't match: passed {}, should be 2 or more.", name, toString(arguments.size()));
-        }
-
-        if (!isStringOrFixedString(arguments.at(0).type))
-        {
-            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type of argument of function {}", name);
-        }
-
-        if (!isStringOrFixedString(arguments.at(1).type) && !isArray(arguments.at(1).type))
-        {
-            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type of argument of function {}", name);
-        }
-
-        if (isStringOrFixedString(arguments.at(1).type))
-        {
-            if (ap == AP_Variadic)
-            {
-                const auto are_arguments_valid = std::ranges::all_of(arguments | std::views::drop(2), [](const auto & argument) { return isStringOrFixedString(argument.type); });
-                if (!are_arguments_valid)
-                    throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type of argument of function {}", name);
-            }
-        }
-
-        else if (ap == AP_Single || !isArray(arguments.at(1).type))
-        {
-            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Illegal type of argument of function {}", name);
-        }
-
-        return std::make_shared<DataTypeUInt8>();
+        throw Exception(
+            ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
+            "Number of arguments for function {} doesn't match: passed {}, should be 2 or more.",
+            name,
+            toString(arguments.size()));
     }
-};
+
+    if (!isStringOrFixedString(arguments.at(0).type))
+    {
+        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type of argument of function {}", name);
+    }
+
+    if (!isStringOrFixedString(arguments.at(1).type) && !isArray(arguments.at(1).type))
+    {
+        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type of argument of function {}", name);
+    }
+
+    if (isStringOrFixedString(arguments.at(1).type))
+    {
+        if constexpr (ap == ArgumentPolicy::Variadic)
+        {
+            const auto are_arguments_valid = std::ranges::all_of(
+                arguments | std::views::drop(2), [](const auto & argument) { return isStringOrFixedString(argument.type); });
+            if (!are_arguments_valid)
+                throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type of argument of function {}", name);
+        }
+    }
+
+    else if (ap == ArgumentPolicy::Single || !isArray(arguments.at(1).type))
+    {
+        throw Exception(ErrorCodes::BAD_ARGUMENTS, "Illegal type of argument of function {}", name);
+    }
+
+    return std::make_shared<DataTypeUInt8>();
+}
 }
