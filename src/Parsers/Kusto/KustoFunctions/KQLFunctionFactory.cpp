c1cf75423c109e8a8018360e591115cdc27a8dad
SadiHassan <mallik.hassan@ibm.com>
2023-02-27 08:10:23 -0800

    lookup() function

--- src/Parsers/Kusto/KustoFunctions/KQLFunctionFactory.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLFunctionFactory.cpp
@@ -214,6 +214,7 @@ enum class KQLFunction : uint16_t
     kase,
     iff,
     iif,
+    lookup,
 
     datatype_bool,
     datatype_datetime,
@@ -459,6 +460,7 @@ const std::unordered_map<String, KQLFunction> KQL_FUNCTIONS{
     {"case", KQLFunction::kase},
     {"iff", KQLFunction::iff},
     {"iif", KQLFunction::iif},
+    {"lookup", KQLFunction::lookup},
 
     {"bool", KQLFunction::datatype_bool},
     {"boolean", KQLFunction::datatype_bool},
@@ -1084,6 +1086,9 @@ std::unique_ptr<IParserKQLFunction> KQLFunctionFactory::get(const String & kql_f
 
         case KQLFunction::iif:
             return std::make_unique<Iif>();
+        
+        case KQLFunction::lookup:
+            return std::make_unique<Lookup>();
 
         case KQLFunction::datatype_bool:
             return std::make_unique<DatatypeBool>();
