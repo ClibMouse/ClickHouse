06f4972c5e61bb63e9a0695f700fcdb1dbd7a865
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2023-03-07 11:16:26 -0800

    Implement default aliasing for KQL print

--- src/Parsers/tests/KQL/gtest_KQL_Dynamic.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_Dynamic.cpp
@@ -8,91 +8,91 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery_DynamicExactMatch, ParserKQLTest,
         ::testing::ValuesIn(std::initializer_list<ParserTestCase>{
         {
             "print array_concat(A, B)",
-            "SELECT arrayConcat(A, B)"
+            "SELECT arrayConcat(A, B) AS print_0"
         },
         {
             "print array_concat(A, B, C, D)",
-            "SELECT arrayConcat(A, B, C, D)"
+            "SELECT arrayConcat(A, B, C, D) AS print_0"
         },
         {
             "print array_iff(A, B, C)",
-            "SELECT kql_ArrayIif(A, B, C)"
+            "SELECT kql_ArrayIif(A, B, C) AS print_0"
         },
         {
-            "print output = array_index_of(dynamic([1, 2, 3]), 2)",
-            "SELECT indexOf([1, 2, 3], 2) - 1 AS output"
+            "print array_index_of(dynamic([1, 2, 3]), 2)",
+            "SELECT indexOf([1, 2, 3], 2) - 1 AS print_0"
         },
         {
-            "print output = array_index_of(dynamic(['a', 'b', 'c']), 'b')",
-            "SELECT indexOf(['a', 'b', 'c'], 'b') - 1 AS output"
+            "print array_index_of(dynamic(['a', 'b', 'c']), 'b')",
+            "SELECT indexOf(['a', 'b', 'c'], 'b') - 1 AS print_0"
         },
         {
-            "print output = array_index_of(dynamic(['John', 'Denver', 'Bob', 'Marley']), 'Marley')",
-            "SELECT indexOf(['John', 'Denver', 'Bob', 'Marley'], 'Marley') - 1 AS output"
+            "print array_index_of(dynamic(['John', 'Denver', 'Bob', 'Marley']), 'Marley')",
+            "SELECT indexOf(['John', 'Denver', 'Bob', 'Marley'], 'Marley') - 1 AS print_0"
         },
         {
             "print array_length(dynamic([1, 2, 3]))",
-            "SELECT arrayLastIndex(x -> true, [1, 2, 3])"
+            "SELECT arrayLastIndex(x -> true, [1, 2, 3]) AS print_0"
         },
         {
             "print array_length(dynamic(['John', 'Denver', 'Bob', 'Marley']))",
-            "SELECT arrayLastIndex(x -> true, ['John', 'Denver', 'Bob', 'Marley'])"
+            "SELECT arrayLastIndex(x -> true, ['John', 'Denver', 'Bob', 'Marley']) AS print_0"
         },
         {
             "print array_reverse(A)",
-            "SELECT arrayReverse(A)"
+            "SELECT arrayReverse(A) AS print_0"
         },
         {
             "print array_rotate_left(A, B)",
-            "SELECT arrayMap(x -> (A[moduloOrZero((x + length(A)) + moduloOrZero(B, toInt64(length(A))), length(A)) + 1]), range(0, length(A)))"
+            "SELECT arrayMap(x -> (A[moduloOrZero((x + length(A)) + moduloOrZero(B, toInt64(length(A))), length(A)) + 1]), range(0, length(A))) AS print_0"
         },
         {
             "print array_rotate_right(A, B)",
-            "SELECT arrayMap(x -> (A[moduloOrZero((x + length(A)) + moduloOrZero(-1 * B, toInt64(length(A))), length(A)) + 1]), range(0, length(A)))"
+            "SELECT arrayMap(x -> (A[moduloOrZero((x + length(A)) + moduloOrZero(-1 * B, toInt64(length(A))), length(A)) + 1]), range(0, length(A))) AS print_0"
         },
         {
             "print array_sum(dynamic([2, 5, 3]))",
-            "SELECT if(multiSearchAny(extract(toTypeName(arrayMap(x -> assumeNotNull(x), arrayFilter(x -> (x IS NOT NULL), [2, 5, 3]))), 'Array\\\\((.*)\\\\)'), ['Bool', 'Decimal', 'Float', 'Int', 'Nothing', 'UInt']), arraySum(x -> toFloat64OrDefault(x), [2, 5, 3]), NULL)"
+            "SELECT if(multiSearchAny(extract(toTypeName(arrayMap(x -> assumeNotNull(x), arrayFilter(x -> (x IS NOT NULL), [2, 5, 3]))), 'Array\\\\((.*)\\\\)'), ['Bool', 'Decimal', 'Float', 'Int', 'Nothing', 'UInt']), arraySum(x -> toFloat64OrDefault(x), [2, 5, 3]), NULL) AS print_0"
         },
         {
             "print array_sum(dynamic([2.5, 5.5, 3]))",
-            "SELECT if(multiSearchAny(extract(toTypeName(arrayMap(x -> assumeNotNull(x), arrayFilter(x -> (x IS NOT NULL), [2.5, 5.5, 3]))), 'Array\\\\((.*)\\\\)'), ['Bool', 'Decimal', 'Float', 'Int', 'Nothing', 'UInt']), arraySum(x -> toFloat64OrDefault(x), [2.5, 5.5, 3]), NULL)"
+            "SELECT if(multiSearchAny(extract(toTypeName(arrayMap(x -> assumeNotNull(x), arrayFilter(x -> (x IS NOT NULL), [2.5, 5.5, 3]))), 'Array\\\\((.*)\\\\)'), ['Bool', 'Decimal', 'Float', 'Int', 'Nothing', 'UInt']), arraySum(x -> toFloat64OrDefault(x), [2.5, 5.5, 3]), NULL) AS print_0"
         },
         {
             "print jaccard_index(A, B)",
-            "SELECT length(arrayIntersect(A, B)) / length(arrayDistinct(arrayConcat(A, B)))"
+            "SELECT length(arrayIntersect(A, B)) / length(arrayDistinct(arrayConcat(A, B))) AS print_0"
         },
         {
             "print pack_array(A, B, C, D)",
-            "SELECT [A, B, C, D]"
+            "SELECT [A, B, C, D] AS print_0"
         },
         {
             "print set_difference(A, B)",
-            "SELECT arrayFilter(x -> (NOT has(arrayDistinct(arrayConcat(B)), x)), arrayDistinct(A))"
+            "SELECT arrayFilter(x -> (NOT has(arrayDistinct(arrayConcat(B)), x)), arrayDistinct(A)) AS print_0"
         },
         {
             "print set_difference(A, B, C)",
-            "SELECT arrayFilter(x -> (NOT has(arrayDistinct(arrayConcat(B, C)), x)), arrayDistinct(A))"
+            "SELECT arrayFilter(x -> (NOT has(arrayDistinct(arrayConcat(B, C)), x)), arrayDistinct(A)) AS print_0"
         },
         {
             "print set_has_element(A, B)",
-            "SELECT has(A, B)"
+            "SELECT has(A, B) AS print_0"
         },
         {
             "print set_intersect(A, B)",
-            "SELECT arrayIntersect(A, B)"
+            "SELECT arrayIntersect(A, B) AS print_0"
         },
         {
             "print set_intersect(A, B, C)",
-            "SELECT arrayIntersect(A, B, C)"
+            "SELECT arrayIntersect(A, B, C) AS print_0"
         },
         {
             "print set_union(A, B)",
-            "SELECT arrayDistinct(arrayConcat(A, B))"
+            "SELECT arrayDistinct(arrayConcat(A, B)) AS print_0"
         },
         {
             "print set_union(A, B, C)",
-            "SELECT arrayDistinct(arrayConcat(A, B, C))"
+            "SELECT arrayDistinct(arrayConcat(A, B, C)) AS print_0"
         }
 })));
 
@@ -102,34 +102,34 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery_DynamicRegex, ParserRegexTest,
         ::testing::ValuesIn(std::initializer_list<ParserTestCase>{
         {
             "print array_shift_left(A, B)",
-            R"(SELECT arrayResize\(if\(B > 0, arraySlice\(A, B \+ 1\), arrayConcat\(arrayWithConstant\(abs\(B\), fill_value_\d+\), A\)\), length\(A\), if\(\(NULL IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)*'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), NULL\) AS fill_value_\d+\))"
+            R"(SELECT arrayResize\(if\(B > 0, arraySlice\(A, B \+ 1\), arrayConcat\(arrayWithConstant\(abs\(B\), fill_value_\d+\), A\)\), length\(A\), if\(\(NULL IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)*'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), NULL\) AS fill_value_\d+\) AS print_0)"
         },
         {
             "print array_shift_left(A, B, C)",
-            R"(SELECT arrayResize\(if\(B > 0, arraySlice\(A, B \+ 1\), arrayConcat\(arrayWithConstant\(abs\(B\), fill_value_\d+\), A\)\), length\(A\), if\(\(C IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), C\) AS fill_value_\d+\))"
+            R"(SELECT arrayResize\(if\(B > 0, arraySlice\(A, B \+ 1\), arrayConcat\(arrayWithConstant\(abs\(B\), fill_value_\d+\), A\)\), length\(A\), if\(\(C IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), C\) AS fill_value_\d+\) AS print_0)"
         },
         {
             "print array_shift_right(A, B)",
-            R"(SELECT arrayResize\(if\(\(-1 \* B\) > 0, arraySlice\(A, \(-1 \* B\) \+ 1\), arrayConcat\(arrayWithConstant\(abs\(-1 \* B\), fill_value_\d+\), A\)\), length\(A\), if\(\(NULL IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), NULL\) AS fill_value_\d+\))"
+            R"(SELECT arrayResize\(if\(\(-1 \* B\) > 0, arraySlice\(A, \(-1 \* B\) \+ 1\), arrayConcat\(arrayWithConstant\(abs\(-1 \* B\), fill_value_\d+\), A\)\), length\(A\), if\(\(NULL IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), NULL\) AS fill_value_\d+\) AS print_0)"
         },
         {
             "print array_shift_right(A, B, C)",
-            R"(SELECT arrayResize\(if\(\(-1 \* B\) > 0, arraySlice\(A, \(-1 \* B\) \+ 1\), arrayConcat\(arrayWithConstant\(abs\(-1 \* B\), fill_value_\d+\), A\)\), length\(A\), if\(\(C IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), C\) AS fill_value_\d+\))"
+            R"(SELECT arrayResize\(if\(\(-1 \* B\) > 0, arraySlice\(A, \(-1 \* B\) \+ 1\), arrayConcat\(arrayWithConstant\(abs\(-1 \* B\), fill_value_\d+\), A\)\), length\(A\), if\(\(C IS NULL\) AND \(\(extract\(toTypeName\(A\), 'Array\\\\\(\(\.\*\)\\\\\)'\) AS element_type_\d+\) = 'String'\), defaultValueOfTypeName\(if\(element_type_\d+ = 'Nothing', 'Nullable\(Nothing\)', element_type_\d+\)\), C\) AS fill_value_\d+\) AS print_0)"
         },
         {
             "print array_slice(A, B, C)",
-            R"(SELECT arraySlice\(A, 1 \+ if\(B >= 0, B, arrayMax\(\[-length\(A\), B\]\) \+ length\(A\)\) AS offset_\d+, \(\(1 \+ if\(C >= 0, C, arrayMax\(\[-length\(A\), C\]\) \+ length\(A\)\)\) - offset_\d+\) \+ 1\))"
+            R"(SELECT arraySlice\(A, 1 \+ if\(B >= 0, B, arrayMax\(\[-length\(A\), B\]\) \+ length\(A\)\) AS offset_\d+, \(\(1 \+ if\(C >= 0, C, arrayMax\(\[-length\(A\), C\]\) \+ length\(A\)\)\) - offset_\d+\) \+ 1\) AS print_0)"
         },
         {
             "print array_split(A, B)",
-            R"(SELECT if\(empty\(arrayMap\(x -> if\(x >= 0, x, arrayMax\(\[0, x \+ CAST\(length\(A\), 'Int\d+'\)\]\)\), flatten\(\[B\]\)\) AS indices_\d+\), \[A\], arrayConcat\(\[arraySlice\(A, 1, indices_\d+\[1\]\)\], arrayMap\(i -> arraySlice\(A, \(indices_\d+\[i\]\) \+ 1, if\(i = length\(indices_\d+\), CAST\(length\(A\), 'Int\d+'\), CAST\(indices_\d+\[i \+ 1\], 'Int\d+'\)\) - \(indices_\d+\[i\]\)\), range\(1, length\(indices_\d+\) \+ 1\)\)\)\))"
+            R"(SELECT if\(empty\(arrayMap\(x -> if\(x >= 0, x, arrayMax\(\[0, x \+ CAST\(length\(A\), 'Int\d+'\)\]\)\), flatten\(\[B\]\)\) AS indices_\d+\), \[A\], arrayConcat\(\[arraySlice\(A, 1, indices_\d+\[1\]\)\], arrayMap\(i -> arraySlice\(A, \(indices_\d+\[i\]\) \+ 1, if\(i = length\(indices_\d+\), CAST\(length\(A\), 'Int\d+'\), CAST\(indices_\d+\[i \+ 1\], 'Int\d+'\)\) - \(indices_\d+\[i\]\)\), range\(1, length\(indices_\d+\) \+ 1\)\)\)\) AS print_0)"
         },
         {
             "print zip(A, B)",
-            R"(SELECT arrayMap\(t -> \[untuple\(t\)\], arrayZip\(arrayResize\(arg0_\d+, arrayMax\(\[length\(if\(match\(toTypeName\(A\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), A, CAST\(A, concat\('Array\(', extract\(toTypeName\(if\(length\(A\) = 0, \[NULL\], A\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg0_\d+\), length\(if\(match\(toTypeName\(B\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), B, CAST\(B, concat\('Array\(', extract\(toTypeName\(if\(length\(B\) = 0, \[NULL\], B\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg1_\d+\)\]\) AS max_length_\d+, NULL\), arrayResize\(arg1_\d+, max_length_\d+, NULL\)\)\))"
+            R"(SELECT arrayMap\(t -> \[untuple\(t\)\], arrayZip\(arrayResize\(arg0_\d+, arrayMax\(\[length\(if\(match\(toTypeName\(A\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), A, CAST\(A, concat\('Array\(', extract\(toTypeName\(if\(length\(A\) = 0, \[NULL\], A\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg0_\d+\), length\(if\(match\(toTypeName\(B\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), B, CAST\(B, concat\('Array\(', extract\(toTypeName\(if\(length\(B\) = 0, \[NULL\], B\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg1_\d+\)\]\) AS max_length_\d+, NULL\), arrayResize\(arg1_\d+, max_length_\d+, NULL\)\)\) AS print_0)"
         },
         {
             "print zip(A, B, C)",
-            R"(SELECT arrayMap\(t -> \[untuple\(t\)\], arrayZip\(arrayResize\(arg0_\d+, arrayMax\(\[length\(if\(match\(toTypeName\(A\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), A, CAST\(A, concat\('Array\(', extract\(toTypeName\(if\(length\(A\) = 0, \[NULL\], A\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg0_\d+\), length\(if\(match\(toTypeName\(B\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), B, CAST\(B, concat\('Array\(', extract\(toTypeName\(if\(length\(B\) = 0, \[NULL\], B\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg1_\d+\), length\(if\(match\(toTypeName\(C\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), C, CAST\(C, concat\('Array\(', extract\(toTypeName\(if\(length\(C\) = 0, \[NULL\], C\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg2_\d+\)\]\) AS max_length_\d+, NULL\), arrayResize\(arg1_\d+, max_length_\d+, NULL\), arrayResize\(arg2_\d+, max_length_\d+, NULL\)\)\))"
+            R"(SELECT arrayMap\(t -> \[untuple\(t\)\], arrayZip\(arrayResize\(arg0_\d+, arrayMax\(\[length\(if\(match\(toTypeName\(A\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), A, CAST\(A, concat\('Array\(', extract\(toTypeName\(if\(length\(A\) = 0, \[NULL\], A\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg0_\d+\), length\(if\(match\(toTypeName\(B\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), B, CAST\(B, concat\('Array\(', extract\(toTypeName\(if\(length\(B\) = 0, \[NULL\], B\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg1_\d+\), length\(if\(match\(toTypeName\(C\), 'Array\\\\\(Nullable\\\\\(\.\*\\\\\)\\\\\)'\), C, CAST\(C, concat\('Array\(', extract\(toTypeName\(if\(length\(C\) = 0, \[NULL\], C\)\), 'Array\\\\\(\(\.\*\)\\\\\)'\), '\)'\)\)\) AS arg2_\d+\)\]\) AS max_length_\d+, NULL\), arrayResize\(arg1_\d+, max_length_\d+, NULL\), arrayResize\(arg2_\d+, max_length_\d+, NULL\)\)\) AS print_0)"
         }
 })));
--- src/Parsers/tests/KQL/gtest_KQL_range.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_range.cpp
@@ -8,63 +8,63 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery_Range, ParserTest,
         ::testing::ValuesIn(std::initializer_list<ParserTestCase>{
         {
             "print range(1, 10, 2)",
-            "SELECT kql_range(1, 10, 2)"
+            "SELECT kql_range(1, 10, 2) AS print_0"
         },
         {
             "print range(1, 10)",
-            "SELECT kql_range(1, 10)"
+            "SELECT kql_range(1, 10) AS print_0"
         },
         {
             "print range(1.2, 10.3, 2.2)",
-            "SELECT kql_range(1.2, 10.3, 2.2)"
+            "SELECT kql_range(1.2, 10.3, 2.2) AS print_0"
         },
         {
             "print range(1.2, 10.3, 2)",
-            "SELECT kql_range(1.2, 10.3, 2)"
+            "SELECT kql_range(1.2, 10.3, 2) AS print_0"
         },
         {
             "print range(1.2, 10,2.2)",
-            "SELECT kql_range(1.2, 10, 2.2)"
+            "SELECT kql_range(1.2, 10, 2.2) AS print_0"
         },
         {
             "print range(1, 10, 2.2)",
-            "SELECT kql_range(1, 10, 2.2)"
+            "SELECT kql_range(1, 10, 2.2) AS print_0"
         },
         {
             "print range(1, 10.5, 2.2)",
-            "SELECT kql_range(1, 10.5, 2.2)"
+            "SELECT kql_range(1, 10.5, 2.2) AS print_0"
         },
         {
             "print range(1.1, 10 ,2.2)",
-            "SELECT kql_range(1.1, 10, 2.2)"
+            "SELECT kql_range(1.1, 10, 2.2) AS print_0"
         },
         {
             "print range(1.2, 10, 2)",
-            "SELECT kql_range(1.2, 10, 2)"
+            "SELECT kql_range(1.2, 10, 2) AS print_0"
         },
         {
             "print range(datetime('2001-01-01'), datetime('2001-01-02'), 5h)",
-            "SELECT kql_range(kql_datetime('2001-01-01'), kql_datetime('2001-01-02'), toIntervalNanosecond(18000000000000))"
+            "SELECT kql_range(kql_datetime('2001-01-01'), kql_datetime('2001-01-02'), toIntervalNanosecond(18000000000000)) AS print_0"
         },
         {
             "print range(datetime('2001-01-01'), datetime('2001-01-02'))",
-            "SELECT kql_range(kql_datetime('2001-01-01'), kql_datetime('2001-01-02'))"
+            "SELECT kql_range(kql_datetime('2001-01-01'), kql_datetime('2001-01-02')) AS print_0"
         },
         {
             "print range(1h, 5h, 2h)",
-            "SELECT kql_range(toIntervalNanosecond(3600000000000), toIntervalNanosecond(18000000000000), toIntervalNanosecond(7200000000000))"
+            "SELECT kql_range(toIntervalNanosecond(3600000000000), toIntervalNanosecond(18000000000000), toIntervalNanosecond(7200000000000)) AS print_0"
         },
         {
             "print range(1.5h, 5h, 2h)",
-            "SELECT kql_range(toIntervalNanosecond(5400000000000), toIntervalNanosecond(18000000000000), toIntervalNanosecond(7200000000000))"
+            "SELECT kql_range(toIntervalNanosecond(5400000000000), toIntervalNanosecond(18000000000000), toIntervalNanosecond(7200000000000)) AS print_0"
         },
         {
             "print range(ago(1d),now(),1d)",
-            "SELECT kql_range(now64(9, 'UTC') + (-1 * toIntervalNanosecond(86400000000000)), now64(9, 'UTC'), toIntervalNanosecond(86400000000000))"
+            "SELECT kql_range(now64(9, 'UTC') + (-1 * toIntervalNanosecond(86400000000000)), now64(9, 'UTC'), toIntervalNanosecond(86400000000000)) AS print_0"
         },
         {
             "print range(endofday(datetime(2017-01-01 10:10:17)), endofday(datetime(2017-01-03 10:10:17)), 1d)",
-            "SELECT kql_range(kql_todatetime(addDays(toStartOfDay(kql_datetime('2017-01-01 10:10:17')), 0 + 1)) - toIntervalNanosecond(100), kql_todatetime(addDays(toStartOfDay(kql_datetime('2017-01-03 10:10:17')), 0 + 1)) - toIntervalNanosecond(100), toIntervalNanosecond(86400000000000))"
+            "SELECT kql_range(kql_todatetime(addDays(toStartOfDay(kql_datetime('2017-01-01 10:10:17')), 0 + 1)) - toIntervalNanosecond(100), kql_todatetime(addDays(toStartOfDay(kql_datetime('2017-01-03 10:10:17')), 0 + 1)) - toIntervalNanosecond(100), toIntervalNanosecond(86400000000000)) AS print_0"
         },
         {
             "range Age from 20 to 25 step 1",
