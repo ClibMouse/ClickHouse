5fa5f6292543cf724d97d4a3c2a4cdd33c7adaa3
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2023-03-07 11:16:26 -0800

    Implement default aliasing for KQL print

--- src/Parsers/tests/KQL/gtest_KQL_Binary.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_Binary.cpp
@@ -8,30 +8,30 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery_Binary, ParserKQLTest,
         ::testing::ValuesIn(std::initializer_list<ParserTestCase>{
         {
             "print binary_and(A, B)",
-            "SELECT bitAnd(CAST(A, 'Int64'), CAST(B, 'Int64'))"
+            "SELECT bitAnd(CAST(A, 'Int64'), CAST(B, 'Int64')) AS print_0"
         },
         {
             "print binary_not(A)",
-            "SELECT bitNot(CAST(A, 'Int64'))"
+            "SELECT bitNot(CAST(A, 'Int64')) AS print_0"
         },
         {
             "print binary_or(A, B)",
-            "SELECT bitOr(CAST(A, 'Int64'), CAST(B, 'Int64'))"
+            "SELECT bitOr(CAST(A, 'Int64'), CAST(B, 'Int64')) AS print_0"
         },
         {
             "print binary_shift_left(A, B)",
-            "SELECT if(B < 0, NULL, bitShiftLeft(CAST(A, 'Int64'), B))"
+            "SELECT if(B < 0, NULL, bitShiftLeft(CAST(A, 'Int64'), B)) AS print_0"
         },
         {
             "print binary_shift_right(A, B)",
-            "SELECT if(B < 0, NULL, bitShiftRight(CAST(A, 'Int64'), B))"
+            "SELECT if(B < 0, NULL, bitShiftRight(CAST(A, 'Int64'), B)) AS print_0"
         },
         {
             "print binary_xor(A, B)",
-            "SELECT bitXor(CAST(A, 'Int64'), CAST(B, 'Int64'))"
+            "SELECT bitXor(CAST(A, 'Int64'), CAST(B, 'Int64')) AS print_0"
         },
         {
             "print bitset_count_ones(A)",
-            "SELECT bitCount(A)"
+            "SELECT bitCount(A) AS print_0"
         }
 })));
