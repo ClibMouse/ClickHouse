f8193dc7099d55145c4361e72c7c871137e19bfc
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2023-03-07 06:47:13 -0800

    Finalize KQL getschema

---
+++ src/Parsers/tests/KQL/gtest_KQL_GetSchema.cpp
@@ -0,0 +1,21 @@
+#include <Parsers/tests/gtest_common.h>
+
+#include <Parsers/Kusto/ParserKQLQuery.h>
+
+INSTANTIATE_TEST_SUITE_P(ParserKQLQuery_GetSchema, ParserTest,
+    ::testing::Combine(
+        ::testing::Values(std::make_shared<DB::ParserKQLQuery>()),
+        ::testing::ValuesIn(std::initializer_list<ParserTestCase>{
+        {
+            "Customers | getschema",
+            "SELECT *\nFROM getschema(\n    SELECT *\n    FROM Customers\n)"
+        },
+        {
+            "Customers | getschema | getschema",
+            "SELECT *\nFROM getschema(\n    SELECT *\n    FROM\n    (\n        SELECT *\n        FROM getschema(\n            SELECT *\n            FROM Customers\n        )\n    )\n)"
+        },
+        {
+            "print x = 'asd' | extend y = strlen(x) | getschema",
+            "SELECT *\nFROM getschema(\n    SELECT *\n    FROM\n    (\n        SELECT\n            * EXCEPT y,\n            lengthUTF8(x) AS y\n        FROM\n        (\n            SELECT 'asd' AS x\n        )\n    )\n)"
+        }
+})));
