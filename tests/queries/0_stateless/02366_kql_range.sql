505f635cd4c4b77a14e9aa55d4ca801521de6692
Yong Wang <yong.wang@ibm.com>
2023-03-11 23:59:42 -0800

    Kusto-phase3: fixed kql range issues

--- tests/queries/0_stateless/02366_kql_range.sql
+++ tests/queries/0_stateless/02366_kql_range.sql
@@ -1,16 +1,85 @@
 set dialect = 'kusto';
 
-print '-- range function int, int, int --';
-print range(1, 10, 2);
-
-print '-- range function int, int --';
+print '-- range function int1 < int2, default int3 --';
 print range(1, 10);
 
-print '-- range function float, float, float --';
+print '-- range function int1 < int2, int3 > 0 --';
+print range(1, 10, 2);
+
+print '-- range function int1 < int2, int3 == 0 --';
+print range(1, 10, 0);
+
+print '-- range function int1 < int2, int3 < 0 --';
+print range(1, 10, -2);
+
+print '-- range function -int1 < int2, int3 > 0 --';
+print range(-1, 10, 2);
+
+print '-- range function int1 > int2 default int3--';
+print range(17, 10);
+
+print '-- range function int1 > int2, int3 > 0 --';
+print range(17, 10, 2);
+
+print '-- range function int1 > int2, int3 == 0 --';
+print range(17, 10, 0);
+
+print '-- range function int1 > int2, int3 < 0 --';
+print range(17, 10, -2);
+
+print '-- range function int1 > -int2, int3 < 0 --';
+print range(17, -10, -2);
+
+print '-- range function int1 == int2 default int3--';
+print range(10, 10);
+
+print '-- range function int1 == int2, int3 > 0 --';
+print range(10, 10, 2);
+
+print '-- range function int1 == int2, int3 == 0 --';
+print range(10, 10, 0);
+
+print '-- range function int1 == int2, int3 < 0 --';
+print range(10, 10, -2);
+
+print '-- range function int1 == int2, float3 > 0 --';
+print range(10, 10, 2.2);
+
+print '-- range function float1 < float2, default step --';
+print range(1.2, 10.3);
+
+print '-- range function float1 < float2, float3 > 0--';
 print range(1.2, 10.3, 2.2);
 
-print '-- range function postive float, float, int --';
-print range(1.2, 10.3, 2);
+print '-- range function  float1 < float2, float3 == 0 --';
+print range(1.2, 10.3, 0);
+
+print '-- range function  float1 < float2, float3 < 0 --';
+print range(1.2, 10.3, -2.2);
+
+print '-- range function float1 > float2, default step --';
+print range(21.2, 10.3);
+
+print '-- range function float1 > float2, float3 > 0--';
+print range(21.2, 10.3, 2.2);
+
+print '-- range function  float1 > float2, float3 == 0 --';
+print range(21.2, 10.3, 0);
+
+print '-- range function  float1 > float2, float3 < 0 --';
+print range(21.2, 10.3, -2.2);
+
+print '-- range function float1 == float2, default step --';
+print range(21.2, 21.2);
+
+print '-- range function float1 == float2, float3 > 0--';
+print range(21.2, 21.2, 2.2);
+
+print '-- range function  float1 == float2, float3 == 0 --';
+print range(21.2, 21.2, 0);
+
+print '-- range function  float1 == float2, float3 < 0 --';
+print range(21.2, 21.2, -2.2);
 
 print '-- range function postive float, int, float --';
 print range(1.2, 10, 2.2);
@@ -42,17 +111,32 @@ print range(datetime('2001-01-01'), datetime('2001-01-02'));
 print '-- range function timespan, timespan, timespan --';
 print range(1h, 5h, 2h);
 
+print '-- range function -timespan, timespan, timespan --';
+print range(-1h, 5h, 2h);
+
 print '-- range function timespan, timespan --';
 print range(1h, 5h);
 
-print '-- range function timespan, timespan, negative timespan --';
+print '-- range function timespan1 > timespan2, negative timespan3 < 0 --';
 print range(11h, 5h, -2h);
 
-print '-- range function float timespan, timespan, timespan --';
+print '-- range function float timespan1 < timespan2, timespan3 > 0 --';
 print range(1.5h, 5h, 2h);
 
-print '-- range function endofday, endofday, timespan --';
+print '-- range function timespan1 < timespan2, timespan3 = 0 --';
+print range(1.5h, 5h, 0h);
+
+print '-- range function timespan1 == timespan2, timespan3 = 0 --';
+print range(5h, 5h, 2h);
+
+print '-- range function datetime1 < datetime2 , timespan > 0 --';
 print range(endofday(datetime(2017-01-01 10:10:17)), endofday(datetime(2017-01-03 10:10:17)), 1d);
+
+print '-- range function datetime1 > datetime2 , timespan > 0 --';
+print range(endofday(datetime(2017-01-05 10:10:17)), endofday(datetime(2017-01-03 10:10:17)), 1d);
+
+print '-- range function datetime1 > datetime2 , timespan < 0 --';
+print range(endofday(datetime(2017-01-05 10:10:17)), endofday(datetime(2017-01-03 10:10:17)), -1d);
 
 print '-- range orerator int, int, int --';
 range Age from 20 to 25 step 1;
