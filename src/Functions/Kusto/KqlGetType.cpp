8ddd0f7b1a9f0456468bbe1bb8cb564027037923
SadiHassan <mallik.hassan@ibm.com>
2023-03-14 14:00:50 -0700

    native version implemented

---
+++ src/Functions/Kusto/KqlGetType.cpp
@@ -0,0 +1,47 @@
+#include <Core/Types.h>
+#include <DataTypes/DataTypeString.h>
+#include <Functions/FunctionFactory.h>
+#include <Functions/FunctionHelpers.h>
+#include <Functions/IFunction.h>
+#include <Parsers/Kusto/KQLDataType.h>
+
+#include <format>
+
+namespace DB
+{
+class FunctionKqlGetType : public IFunction
+{
+public:
+    static constexpr auto name = "kql_gettype";
+    static FunctionPtr create(ContextPtr context) { return std::make_shared<FunctionKqlGetType>(std::move(context)); }
+
+    explicit FunctionKqlGetType(ContextPtr context_) : context(std::move(context_)) { }
+    ~FunctionKqlGetType() override = default;
+
+    ColumnPtr
+    executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & result_type, size_t input_rows_count) const override;
+    String getName() const override { return name; }
+    size_t getNumberOfArguments() const override { return 1; }
+    DataTypePtr getReturnTypeImpl(const DataTypes &) const override { return std::make_shared<DataTypeString>(); }
+    bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return false; }
+
+private:
+    ContextPtr context;
+};
+
+ColumnPtr
+FunctionKqlGetType::executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, const size_t input_rows_count) const
+{
+    const auto & argument = arguments.front();
+
+    WhichDataType which_data_type(*argument.type);
+
+    return DataTypeString().createColumnConst(input_rows_count, toString(toKQLDataType(which_data_type.idx)));
+}
+
+
+REGISTER_FUNCTION(KqlGetType)
+{
+    factory.registerFunction<FunctionKqlGetType>();
+}
+}
