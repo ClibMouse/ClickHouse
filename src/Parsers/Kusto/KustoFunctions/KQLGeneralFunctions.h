84cbeee011cf97551e0657823b08420114ab607d
SadiHassan <mallik.hassan@ibm.com>
2023-03-14 14:09:20 -0700

    updated tests

--- src/Parsers/Kusto/KustoFunctions/KQLGeneralFunctions.h
+++ src/Parsers/Kusto/KustoFunctions/KQLGeneralFunctions.h
@@ -8,48 +8,48 @@ class Bin : public IParserKQLFunction
 {
 protected:
     const char * getName() const override { return "bin()"; }
-    bool convertImpl(String &out,IParser::Pos &pos) override;
+    bool convertImpl(String & out, IParser::Pos & pos) override;
 };
 
 class BinAt : public IParserKQLFunction
 {
 protected:
     const char * getName() const override { return "bin_at()"; }
-    bool convertImpl(String &out,IParser::Pos &pos) override;
+    bool convertImpl(String & out, IParser::Pos & pos) override;
 };
 
 class Case : public IParserKQLFunction
 {
 protected:
     const char * getName() const override { return "case()"; }
-    bool convertImpl(String &out,IParser::Pos &pos) override;
+    bool convertImpl(String & out, IParser::Pos & pos) override;
 };
 
 class Iff : public IParserKQLFunction
 {
 protected:
     const char * getName() const override { return "iff()"; }
-    bool convertImpl(String &out,IParser::Pos &pos) override;
+    bool convertImpl(String & out, IParser::Pos & pos) override;
 };
 class Iif : public IParserKQLFunction
 {
 protected:
     const char * getName() const override { return "iif()"; }
-    bool convertImpl(String &out,IParser::Pos &pos) override;
+    bool convertImpl(String & out, IParser::Pos & pos) override;
 };
 
 class Lookup : public IParserKQLFunction
 {
 protected:
     const char * getName() const override { return "lookup()"; }
-    bool convertImpl(String &out,IParser::Pos &pos) override;
+    bool convertImpl(String & out, IParser::Pos & pos) override;
 };
 
 class GetType : public IParserKQLFunction
 {
 protected:
     const char * getName() const override { return "gettype()"; }
-    bool convertImpl(String &out,IParser::Pos &pos) override;
+    bool convertImpl(String & out, IParser::Pos & pos) override;
 };
 
 }
