5789bb1a5ee8c62948abb7e8cae0e125ed588f67
mcmajam <mcmahonj@us.ibm.com>
2023-03-14 11:19:52 -0700

    Core review changes Hash changes
    Small correction
    
    code review changes

--- src/Parsers/Kusto/KustoFunctions/KQLFunctionFactory.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLFunctionFactory.cpp
@@ -6,6 +6,7 @@
 #include "KQLDateTimeFunctions.h"
 #include "KQLDynamicFunctions.h"
 #include "KQLGeneralFunctions.h"
+#include "KQLHashFunctions.h"
 #include "KQLIPFunctions.h"
 #include "KQLMathematicalFunctions.h"
 #include "KQLStringFunctions.h"
@@ -264,7 +265,10 @@ enum class KQLFunction : uint16_t
     sign,
     sin,
     sqrt,
-    tan
+    tan,
+
+    hash,
+    hash_sha256
 };
 
 const std::unordered_map<String, KQLFunction> KQL_FUNCTIONS{
@@ -526,7 +530,10 @@ const std::unordered_map<String, KQLFunction> KQL_FUNCTIONS{
     {"sign", KQLFunction::sign},
     {"sin", KQLFunction::sin},
     {"sqrt", KQLFunction::sqrt},
-    {"tan", KQLFunction::tan}};
+    {"tan", KQLFunction::tan},
+
+    {"hash", KQLFunction::hash},
+    {"hash_sha256", KQLFunction::hash_sha256}};
 }
 
 namespace DB
@@ -537,7 +544,7 @@ std::unique_ptr<IParserKQLFunction> KQLFunctionFactory::get(const String & kql_f
     if (kql_function_it == KQL_FUNCTIONS.end())
         return nullptr;
 
-    const auto& kql_function_id = kql_function_it->second;
+    const auto & kql_function_id = kql_function_it->second;
     switch (kql_function_id)
     {
         case KQLFunction::none:
@@ -1253,6 +1260,12 @@ std::unique_ptr<IParserKQLFunction> KQLFunctionFactory::get(const String & kql_f
 
         case KQLFunction::tan:
             return std::make_unique<Tan>();
+
+        case KQLFunction::hash:
+            return std::make_unique<Hash>();
+
+        case KQLFunction::hash_sha256:
+            return std::make_unique<HashSha256>();
     }
 }
 }
