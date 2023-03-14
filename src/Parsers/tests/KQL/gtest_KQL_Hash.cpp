f5486c2d938681671aa1132d1baac3b0478cb388
mcmajam <mcmahonj@us.ibm.com>
2023-03-14 13:04:03 -0700

    Fix test

--- src/Parsers/tests/KQL/gtest_KQL_Hash.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_Hash.cpp
@@ -8,14 +8,14 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery_Hash, ParserTest,
         ::testing::ValuesIn(std::initializer_list<ParserTestCase>{
         {
             "print hash('World')",
-            "SELECT xxHash64KQL('World')"
+            "SELECT kql_hash('World') AS print_0"
         },
         {
             "print hash('World', 100)",
-            "SELECT if(100 < 1, throwIf(true, 'hash(): argument 2 must be a constant positive long value'), toUInt64(xxHash64KQL('World')) % 100)"
+            "SELECT if(100 < 1, throwIf(true, 'hash(): argument 2 must be a constant positive long value'), toUInt64(kql_hash('World')) % 100) AS print_0"
         },
         {
             "print hash_sha256('World')",
-            "SELECT hex(SHA256(ifNull(kql_tostring('World'), '')))"
+            "SELECT hex(SHA256(ifNull(kql_tostring('World'), ''))) AS print_0"
         },
 })));
