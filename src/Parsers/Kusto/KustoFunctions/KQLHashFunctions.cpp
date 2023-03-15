73e40239714789c9cfba40e0ad75db8c652cb26a
mcmajam <mcmahonj@us.ibm.com>
2023-03-15 09:25:42 -0700

    additional code review changes

--- src/Parsers/Kusto/KustoFunctions/KQLHashFunctions.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLHashFunctions.cpp
@@ -6,18 +6,7 @@ namespace DB
 {
 bool Hash::convertImpl(String & out, IParser::Pos & pos)
 {
-    const auto fn_name = getKQLFunctionName(pos);
-
-    if (fn_name.empty())
-        return false;
-    const auto arg = getArgument(fn_name, pos);
-    const auto mod = getOptionalArgument(fn_name, pos);
-    out = std::format(
-        "{0}kql_hash({1} {2}",
-        mod ? "if(" + mod.value() + " < 1, throwIf(true, 'hash(): argument 2 must be a constant positive long value'), toUInt64(" : "",
-        arg,
-        mod ? ")) % " + mod.value() + ")" : ")");
-    return true;
+    return directMapping(out, pos, "kql_hash");
 }
 
 bool HashSha256::convertImpl(String & out, IParser::Pos & pos)
@@ -27,7 +16,7 @@ bool HashSha256::convertImpl(String & out, IParser::Pos & pos)
         return false;
 
     const auto arg = getArgument(function_name, pos, ArgumentState::Raw);
-    out = "hex(SHA256(" + kqlCallToExpression("tostring", {arg}, pos.max_depth) + "))";
+    out = "lower(hex(SHA256(" + kqlCallToExpression("tostring", {arg}, pos.max_depth) + ")))";
 
     return true;
 }
