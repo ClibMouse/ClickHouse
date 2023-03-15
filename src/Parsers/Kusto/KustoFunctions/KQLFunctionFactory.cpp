38a1111b4e14893e6c6d5bebff91ebd0693a03d0
SadiHassan <mallik.hassan@ibm.com>
2023-03-15 01:39:55 -0700

    style correction

--- src/Parsers/Kusto/KustoFunctions/KQLFunctionFactory.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLFunctionFactory.cpp
@@ -1126,7 +1126,7 @@ std::unique_ptr<IParserKQLFunction> KQLFunctionFactory::get(const String & kql_f
 
         case KQLFunction::lookup:
             return std::make_unique<Lookup>();
-        
+
         case KQLFunction::gettype:
             return std::make_unique<GetType>();
 
