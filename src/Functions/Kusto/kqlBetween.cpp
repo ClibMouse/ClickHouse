36e7c2cece78cc27d4936358b56696b1931da7d3
SadiHassan <mallik.hassan@ibm.com>
2023-03-16 18:52:13 -0700

    solved floating point parsing issue by changing Lexer.cpp, but this change might not see the light in OSS

--- src/Functions/Kusto/kqlBetween.cpp
+++ src/Functions/Kusto/kqlBetween.cpp
@@ -1,18 +1,17 @@
-#include <DataTypes/DataTypesNumber.h>
+#include <Columns/ColumnsNumber.h>
+#include <DataTypes/DataTypeFactory.h>
 #include <DataTypes/DataTypeString.h>
+#include <DataTypes/DataTypesNumber.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionHelpers.h>
 #include <Functions/IFunction.h>
 #include <Parsers/Kusto/ParserKQLTimespan.h>
-#include <DataTypes/DataTypeFactory.h>
-#include <Columns/ColumnsNumber.h>
 
 
 #include <format>
 
 namespace DB
 {
-
 namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
@@ -38,68 +37,48 @@ public:
 private:
     ContextPtr context;
 };
-/*
-TO DO Steps
-1. make it work for normal values (int, decimal, DateTime etc...)
-2. for DateTime and TimeSpan or other, make special case and make it work
-3. Fix code in Operator cpp file --- add check for dots, params, brackets etc. and throw exception
-4. make a new brunch in Kusto/ folder from latest phase3
-5. Copy code from here to there
-4. Run functional and unit test
-5. Fix/Add test cases
-6. Run all tests again
-7. Submit a new PR
-*/
+
 ColumnPtr
 FunctionKqlBetween::executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, const size_t input_rows_count) const
 {
     if (arguments.size() < 3)
         throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Number of arguments in function {} doesn't match.", getName());
-    const auto & base_arg = arguments[0]; // X
-    const auto & comparable_arg1 = arguments[1]; // Y
-    const auto & comparable_arg2 = arguments[2]; // Z  x >= y and x <= y + z
-    
+
+    const auto & base_arg = arguments[0];
+    const auto & comparable_arg1 = arguments[1];
+    const auto & comparable_arg2 = arguments[2];
+
     auto new_column = ColumnUInt8::create(input_rows_count);
-    
+
     bool add_interval = false;
-    
-    if (WhichDataType which_data_type(*comparable_arg2.type);which_data_type.isInterval())
+
+    if (WhichDataType which_data_type(*comparable_arg2.type); which_data_type.isInterval())
         add_interval = true;
-    std::cout << base_arg.column->getFamilyName() << std::endl;
-    std::cout << base_arg.column->getName() << std::endl;
-    //std::cout << base_arg.type->getTypeId() << std::endl;
-    std::cout << "---------------------------" << std::endl;
-    
-    std::cout << comparable_arg1.column->getFamilyName() << std::endl;
-    std::cout << comparable_arg1.column->getName() << std::endl;
-    std::cout << "---------------------------" << std::endl;
-    std::cout << comparable_arg2.column->getFamilyName() << std::endl;
-    std::cout << comparable_arg2.column->getName() << std::endl;
-    std::cout << "..........................." << std::endl;
-    
+
     for (size_t i = 0; i < input_rows_count; ++i)
     {
-        std::cout << "base_arg << (" << i << ") : " << base_arg.column->getInt(i) << std::endl; 
-        std::cout << "comparable_arg1 << (" << i << ") : " << comparable_arg1.column->getInt(i) << std::endl; 
-        std::cout << "comparable_arg2 << (" << i << ") : " << comparable_arg2.column->getInt(i) << std::endl; 
         if (add_interval)
-            new_column->getData()[i] = (base_arg.column->getInt(i) >= comparable_arg1.column->getInt(i) && base_arg.column->getInt(i) <= comparable_arg2.column->getInt(i) + comparable_arg1.column->getInt(i));
+            new_column->getData()[i]
+                = (base_arg.column->getInt(i) >= comparable_arg1.column->getInt(i)
+                   && base_arg.column->getInt(i) <= comparable_arg2.column->getInt(i) + comparable_arg1.column->getInt(i));
         else
         {
-            if (WhichDataType which_data_type2(*base_arg.type); which_data_type2.isInt() || which_data_type2.isDateOrDate32OrDateTimeOrDateTime64())
-                new_column->getData()[i] = (base_arg.column->getInt(i) >= comparable_arg1.column->getInt(i) && base_arg.column->getInt(i) <= comparable_arg2.column->getInt(i));
+            if (WhichDataType which_data_type2(*base_arg.type);
+                which_data_type2.isInt() || which_data_type2.isDateOrDate32OrDateTimeOrDateTime64())
+                new_column->getData()[i]
+                    = (base_arg.column->getInt(i) >= comparable_arg1.column->getInt(i)
+                       && base_arg.column->getInt(i) <= comparable_arg2.column->getInt(i));
             else if (WhichDataType which_data_type3(*base_arg.type); which_data_type3.isUInt())
-                new_column->getData()[i] = (base_arg.column->getUInt(i) >= comparable_arg1.column->getUInt(i) && base_arg.column->getUInt(i) <= comparable_arg2.column->getUInt(i));
+                new_column->getData()[i]
+                    = (base_arg.column->getUInt(i) >= comparable_arg1.column->getUInt(i)
+                       && base_arg.column->getUInt(i) <= comparable_arg2.column->getUInt(i));
             else if (WhichDataType which_data_type4(*base_arg.type); which_data_type4.isDecimal() || which_data_type4.isFloat())
-                new_column->getData()[i] = (base_arg.column->getFloat64(i) >= comparable_arg1.column->getFloat64(i) && base_arg.column->getFloat64(i) <= comparable_arg2.column->getFloat64(i));
+                new_column->getData()[i]
+                    = (base_arg.column->getFloat64(i) >= comparable_arg1.column->getFloat64(i)
+                       && base_arg.column->getFloat64(i) <= comparable_arg2.column->getFloat64(i));
         }
-            
     }
-   
-    std::cout << "Inside FunctionKqlBetween" << std::endl;
-    //return executeFunctionCall(context, "toUInt8", arguments, input_rows_count).first;
-    
-    //return executeFunctionCall(context, "and", {base_arg > comparable_arg1, base_arg < comparable_arg1}, input_rows_count).first;
+
     return new_column;
 }
 
