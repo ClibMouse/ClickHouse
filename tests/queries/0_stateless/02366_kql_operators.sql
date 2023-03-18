a0856678de3a21b01a2398b8a5863b61c732b7b2
SadiHassan <mallik.hassan@ibm.com>
2023-03-17 17:30:10 -0700

    updated native function + parser

---
+++ tests/queries/0_stateless/02366_kql_operators.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS TableWithVariousDataTypes;
+CREATE TABLE TableWithVariousDataTypes
+(
+    Name String,
+    Age Nullable(UInt8),
+    Height Float64,
+    JoinDate DateTime64(9, 'UTC')
+) engine = Memory;
+
+INSERT INTO TableWithVariousDataTypes VALUES ('A', 12, 5.2, '2020-01-01'), ('B', 22, 7.2, '2020-01-02'), ('C', 32, 9.3, '2021-12-31');
+
+set dialect='kusto';
+
+print '-- operator between, !between --';
+TableWithVariousDataTypes | project Age | where Age between (10 .. 12);
+TableWithVariousDataTypes | project Age | where Age !between (10 .. 30);
+TableWithVariousDataTypes | project Height | where Height between (5.2 .. 6.6);
+TableWithVariousDataTypes | project Height | where Height !between (5.3 .. 7.6);
+TableWithVariousDataTypes | project JoinDate | where todatetime(JoinDate) between (datetime('2020-01-01') .. 2d);
+TableWithVariousDataTypes | project JoinDate | where JoinDate !between (datetime('2020-01-01') .. 2d);
+TableWithVariousDataTypes | project JoinDate | where JoinDate between (datetime('2020-06-30') .. datetime('2025-06-30'));
+TableWithVariousDataTypes | project JoinDate | where JoinDate !between (datetime('2020-06-30') .. datetime('2025-06-30'));
+TableWithVariousDataTypes | project Age | where Age between (10 .. 12) or Age > 30;
+TableWithVariousDataTypes | project Age | where Age between (10 .. 12) or Age between (30 .. 50);
+range x from 1 to 100 step 1 | where x between ( 8 + (4 * 2) / 8 + ((5 * 5 - 20) * 2) .. 20);
