a8944fcf75fd84e66b23033c2d7086169c86b498
SadiHassan <mallik.hassan@ibm.com>
2023-03-31 11:59:55 -0700

    addressed review comments

--- tests/queries/0_stateless/02366_kql_func_string.sql
+++ tests/queries/0_stateless/02366_kql_func_string.sql
@@ -490,5 +490,5 @@ print str = make_string(range(80, 85), 86, 87, range(88, 90));
 print str = make_string(dynamic([]), 80, 81);
 print str = make_string(123, 1605, 29233 ,128002, 2437);
 
--- print t = new_guid(1)
--- Code: 36. DB::Exception: Function new_guid expects 0 argument(s). (BAD_ARGUMENTS)
+print '-- new_guid --';
+print t = new_guid(1) -- { clientError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
