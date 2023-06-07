15ef845bb41875276c6fb9b95f7d0d4887563c80
umang brahmbhatt <umang8223@gmail.com>
2023-06-06 21:04:32 -0400

    swapping commands

--- tests/ci/run_check.py
+++ tests/ci/run_check.py
@@ -5,6 +5,15 @@ from typing import Tuple
 
 from github import Github
 
+from docs_check import NAME as DOCS_NAME
+from env_helper import GITHUB_REPOSITORY, GITHUB_SERVER_URL
+from get_robot_token import get_best_robot_token
+from pr_info import FORCE_TESTS_LABEL, PRInfo
+from lambda_shared_package.lambda_shared.pr import (
+    CATEGORY_TO_LABEL,
+    TRUSTED_CONTRIBUTORS,
+    check_pr_description,
+)
 from commit_status_helper import (
     CI_STATUS_NAME,
     NotSet,
@@ -16,16 +25,6 @@ from commit_status_helper import (
     remove_labels,
     set_mergeable_check,
 )
-from docs_check import NAME as DOCS_NAME
-from get_robot_token import get_best_robot_token
-from env_helper import GITHUB_REPOSITORY, GITHUB_SERVER_URL
-from pr_info import FORCE_TESTS_LABEL, PRInfo
-from lambda_shared_package.lambda_shared.pr import (
-    CATEGORY_TO_LABEL,
-    TRUSTED_CONTRIBUTORS,
-    check_pr_description,
-)
-
 TRUSTED_ORG_IDS = {
     54801242,  # clickhouse
 }
