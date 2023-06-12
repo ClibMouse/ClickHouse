35b9b1e432a2f4168fd85f1cbc61d8efeee67390
umang brahmbhatt <umang8223@gmail.com>
2023-06-12 09:07:01 -0400

    trying for SCCACHE_S3_NO_CREDENTIALS

--- tests/ci/fast_test_check.py
+++ tests/ci/fast_test_check.py
@@ -58,6 +58,7 @@ def get_fasttest_cmd(workspace, output_path, repo_path, pr_number, commit_sha, i
         f"-e COPY_CLICKHOUSE_BINARY_TO_OUTPUT=1 "
         f"-e SCCACHE_S3_USE_SSL=true "
         f"-e SCCACHE_BUCKET={S3_BUILDS_BUCKET} -e SCCACHE_S3_KEY_PREFIX=ccache/sccache "
+        f"-e SCCACHE_S3_NO_CREDENTIALS=true "
         # f"-e IBM_COS_API_KEY={IBM_COS_API_KEY} "
         # f"-e IBM_COS_INSTANCE_CRN={IBM_COS_INSTANCE_CRN} "
         f"-e SCCACHE_REGION={S3_REGION} "
