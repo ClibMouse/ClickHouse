c2b062d7feb115633210e98181b807d6145f41cc
umang brahmbhatt <umang8223@gmail.com>
2023-07-18 10:22:00 -0400

    solved circular import error

--- tests/ci/get_robot_token.py
+++ tests/ci/get_robot_token.py
@@ -8,7 +8,7 @@ import hvac
 from github import Github
 from github.AuthenticatedUser import AuthenticatedUser
 
-from contants import VAULT_URL, VAULT_TOKEN, VAULT_PATH, VAULT_MOUNT_POINT
+from constants import VAULT_URL, VAULT_TOKEN, VAULT_PATH, VAULT_MOUNT_POINT
 
 @dataclass
 class Token:
